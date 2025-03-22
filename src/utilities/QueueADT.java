00e2f1fe87bb41cb2f3b08ee0f23eca0890cf8ce
eevnyx <onep1x.11yt@gmail.com>
2025-03-21 20:52:30 -0600

    updated/added javadocs to QueueADT.java

--- src/utilities/QueueADT.java
+++ src/utilities/QueueADT.java
@@ -32,6 +32,7 @@ public interface QueueADT<E> {
      * @pre The queue is not empty.
      * @return The element at the front of the queue
      * @throws EmptyQueueException if the queue is empty
+	 * @post The queue remains unchanged
      */
 	E peek();
 	
@@ -39,6 +40,7 @@ public interface QueueADT<E> {
 	 * Returns true if queue is empty, false if not
 	 * 
 	 * @return True if the queue is empty, false if not
+	 * @post The queue remains unchanged
 	 */
 	boolean isEmpty();
 	
@@ -46,13 +48,14 @@ public interface QueueADT<E> {
 	 * Returns number of elements currently in the queue
 	 * 
 	 * @return the number of elements in the queue
+	 * @post The queue remains unchanged
 	 */
 	int size();
 	
 	/**
 	 * Removes all elements from the queue
 	 * 
-	 * @post the queue is empty
+	 * @post The queue is empty
 	 */
 	void dequeueAll();
 	
@@ -62,26 +65,37 @@ public interface QueueADT<E> {
 	 * 
 	 * @param other, the other queue to compare
 	 * @return true if the queues are equal, false if not
+	 * @post The queue remains unchanged
 	 */
 	boolean equals(QueueADT<?> other);
 	
 	/**
-	 * 
-	 * @return
-	 */
+     * Returns an iterator over the elements in this queue
+	 * in proper sequence from front to back.
+     * 
+     * @return an iterator over the elements in this queue
+	 * @post The queue remains unchanged
+     */
 	utilities.Iterator<E> iterator();
 	
 	/**
-	 * 
-	 * @param copy
-	 * @return
-	 */
+     * Returns an array containing all of the elements in this queue 
+	 * in proper front to back sequence
+     * The runtime type of the returned array is that of the specified array.
+     * 
+     * @param copy the array into which the elements are to be stored, if it is large enough,
+     *             otherwise, a new array of the same runtime type is allocated
+     * @return an array containing all of the elements in this queue
+     * @throws NullPointerException if the specified array is null
+     * @post The queue remains unchanged
+     */
 	E[] toArray(E[] copy);
 	
 	/**
 	 * Returns true if the queue is full, false if not
 	 * 
 	 * @return true if the queue is full, false if not
+	 * @post The queue remains unchanged
 	 */
 	boolean isFull();
 	
@@ -91,13 +105,19 @@ public interface QueueADT<E> {
 	 * @param obj, element to check the queue for
 	 * @return true if queue contains specified element
 	 * @throws NullPointerException if the specified element is null
+	 * @post The queue remains unchanged
 	 */
 	boolean contains(E obj);
 	
 	/**
-	 * 
-	 * @param element
-	 * @return
-	 */
+     * Returns the 1-based position where an element is in this queue.
+     * If the element appears multiple times, the position closest to the front is returned.
+     * 
+     * @param element the element to search for
+     * @return the 1-based position from the front of the queue where the element is located,
+     *         -1 if the element is not found
+     * @throws NullPointerException if the specified element is null
+     * @post The queue remains unchanged
+     */
 	int search(E element);
 }
